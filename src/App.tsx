31824e433f5e166d3ff2917cc5843bbce349d815
pdmurray <peynmurray@gmail.com>
2026-02-28 11:53:03 -0800

    Add pre commit, biome configs. Strip out unused boilerplate; add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
-import { useState, useEffect } from 'react'
-import SplatFactory, { type MainModule } from './wasm/splat';
-import './App.css'
+import { AppShell, Burger, MantineProvider } from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks"
+import { useEffect, useState } from "react"
+import { theme } from "./theme"
+import SplatFactory, { type MainModule } from "./wasm/splat"
+import "@mantine/core/styles.css"
 
-function App() {
-  const [splatModule, setSplatModule] = useState<MainModule | null>(null);
+export default function App() {
+  const [splatModule, setSplatModule] = useState<MainModule | null>(null)
   useEffect(() => {
     SplatFactory({ noInitialRun: true }).then((mod) => setSplatModule(mod))
   }, [])
 
   if (splatModule !== null) {
-    console.log(splatModule);
+    console.log(splatModule)
   }
 
+  const [opened, { toggle }] = useDisclosure()
+
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <MantineProvider theme={theme}>
+      <AppShell
+        padding="md"
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+      >
+        <AppShell.Header>
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+
+          <div>Logo</div>
+        </AppShell.Header>
+
+        <AppShell.Navbar>Navbar</AppShell.Navbar>
+
+        <AppShell.Main>Main</AppShell.Main>
+      </AppShell>
+    </MantineProvider>
   )
 }
-
-export default App
