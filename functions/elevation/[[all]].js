01b3fba1979cee791792265837646c9d327278c5
pdmurray <peynmurray@gmail.com>
2026-03-13 15:58:32 -0700

    Remove all CORS headers stuff

--- functions/elevation/[[all]].js
+++ functions/elevation/[[all]].js
@@ -1,13 +1,5 @@
 export async function onRequestGet(ctx) {
   const path = new URL(ctx.request.url).pathname.replace("/elevation/", "")
-  const origin = ctx.request.headers.get("origin")
-  if (origin === null) {
-    console.log(`Origin is null: ${JSON.stringify(ctx.request)}`)
-    return new Response(`Origin is null: ${JSON.stringify(ctx.request)}`, {
-      status: 404,
-    })
-  }
-
   const file = await ctx.env.SRTMGL3S_BUCKET.get(path)
   if (!file) {
     console.log(`File doesn't exist: ${JSON.stringify(ctx.request)}`)
@@ -16,28 +8,7 @@ export async function onRequestGet(ctx) {
     })
   }
 
-  const allowedOrigins = [
-    /https:\/\/waveguide\.dev/,
-    /https:\/\/.*\.waveguide-dev\.pages\.dev/,
-  ]
-
-  for (const pattern of allowedOrigins) {
-    if (origin.match(pattern)) {
-      return new Response(file.body, {
-        headers: {
-          "Content-Type": file.httpMetadata.contentType,
-          "Access-Control-Allow-Origin": origin,
-          "Access-Control-Allow-Methods": "GET",
-        },
-      })
-    }
-  }
-
-  console.log(
-    `Origin doesn't match valid origins: ${JSON.stringify(ctx.request)}`,
-  )
-  return new Response(
-    `Origin doesn't match valid origins: ${JSON.stringify(ctx.request)}`,
-    { status: 404 },
-  )
+  return new Response(file.body, {
+    headers: { "Content-Type": file.httpMetadata.contentType },
+  })
 }
